568be5f08d5c8bef7db5fbfa498496634cc35b59
Ethan Shealey <ethan.shealey@gmail.com>
2021-03-25 23:09:44 -0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:######@127.0.0.1/flask_music_app"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:pass@127.0.0.1/flask_music_app"
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.config.from_object(Config)
